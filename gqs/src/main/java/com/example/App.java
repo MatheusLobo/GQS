b223219f484404ea33e0e7b510fdc18b1905e944
MatheusLobo <mat_lo@hotmail.com>
2024-03-26 20:09:12 -0300

    arrumando o pom

---
+++ gqs/src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Olá, Mundo testando unifacs!" );
+    }
+}
---
+++ gqs/src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest 
+{
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
