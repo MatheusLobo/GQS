0bf854c1fd3d1e8e4e60cb935a7e7792edbef816
MatheusLobo <mat_lo@hotmail.com>
2024-03-28 16:58:59 -0300

    docs: update release.config e commitlint

--- gqs/src/main/java/com/example/App.java
+++ gqs/src/main/java/com/example/App.java
@@ -7,6 +7,8 @@ public class App
     	System.out.println( "Olá, Mundo testando unifacs teste 5!" );
     	System.out.println( "Olá, Mundo testando unifacs teste 5!" );
     	System.out.println( "Olá, Mundo testando unifacs teste 5!" );
+    	System.out.println( "Olá, Mundo testando unifacs teste 5!" );
+    	System.out.println( "Olá, Mundo testando unifacs teste 5!" );
     
     }
 }
