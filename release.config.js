93afaac64c3bf1b2f396dc4c878a90a68d49b422
MatheusLobo <mat_lo@hotmail.com>
2024-03-27 23:05:57 -0300

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,10 +1,19 @@
 module.exports = {
-  branches: ['main'], // Branch monitorar
+  branches: ['main'], // Branch monitorada
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/npm',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true,
+        pkgRoot: './dist',
+        tarballDir: './dist',
+        // Aqui você adiciona o token do npm como variável de ambiente
+        npmToken: process.env.NPM_TOKEN,
+      },
+    ],
     '@semantic-release/git',
   ],
 };
